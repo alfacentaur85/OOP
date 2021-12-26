2676d2882171f270019956218d76edd62ba7dde1
Artur Stepanyants <alfacentaur@yandex.ru>
2021-12-26 20:28:56 +0300

    lesson2

---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccountChecking = new BankAccount(AccountTypeEnum.Checking, 0);
+            BankAccount bankAccountDeposit = new BankAccount(AccountTypeEnum.Deposit, 1000);
+            BankAccount bankAccountPayment = new BankAccount(AccountTypeEnum.Payment, 5000);
+
+            bankAccountDeposit.DepositToAccount(2000);
+
+            bankAccountDeposit.WithdrawFromAccount(4000);
+
+            bankAccountDeposit.WithdrawFromAccount(3000);
+
+        }
+    }
+}
