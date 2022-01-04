a48e18dc25700b59fd37536f2088d2c1e6f3441c
Artur Stepanyants <alfacentaur@yandex.ru>
2022-01-04 14:33:04 +0300

    lesson3 mod 2

--- Lesson3/Lesson3/Program.cs
+++ Lesson3/Lesson3/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 
-namespace Lesson3
+namespace Lesson2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount bankAccountCheckingFrom = new BankAccount(AccountTypeEnum.Checking, 1000);
+            BankAccount bankAccountDeposit = new BankAccount(AccountTypeEnum.Deposit, 0);
+
+            bankAccountDeposit.WithdrawFromAccount(bankAccountCheckingFrom, 4000);
+
+            bankAccountDeposit.WithdrawFromAccount(bankAccountCheckingFrom, 500);
+
+
         }
     }
 }
---
+++ Lesson3/StringReverse/StringUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReverse
+{
+    public static class StringUtils
+    {
+        public static string StringReverse(string str)
+        {
+            string result = "";
+            if (!string.IsNullOrEmpty(str))
+            {
+                for(int i = str.Length - 1; i >= 0; i--)
+                {
+                    result += str[i];
+                }
+            }
+            return result;
+        }
+    }
+}
