b032847c06e5debc6f80b3ea803ca5678073e30b
Artur Stepanyants <alfacentaur@yandex.ru>
2022-01-16 22:49:11 +0300

    lesson5 mod 2

--- Lesson5/Lesson5/Program.cs
+++ Lesson5/Lesson5/Program.cs
@@ -6,7 +6,51 @@ namespace Lesson5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rational r1 = new Rational(3, 5);
+            Rational r2 = new Rational(7, 6);
+            Rational r3 = new Rational(6, 10);
+            Rational r4 = new Rational(12, 10);
+            Rational r5 = new Rational(1, 10);
+
+            Console.WriteLine(r1.ToString());
+
+            
+            Console.WriteLine(r1.Equals(r2));
+            Console.WriteLine(r1.Equals(r3));
+
+            Console.WriteLine((r1 + r2).ToString());
+
+            Console.WriteLine((r1 - r2).ToString());
+
+            Console.WriteLine((r1 * r2).ToString());
+
+            Console.WriteLine((r1 / r2).ToString());
+
+            Console.WriteLine((r1 > r2));
+
+            Console.WriteLine(r1 < r2);
+
+            Console.WriteLine(r1 >= r3);
+
+            Console.WriteLine(r1 <= r3);
+            
+            Rational r = r1++;
+            Console.WriteLine(r.ToString());
+            Console.WriteLine(r1.ToString());
+
+            r = r4--;
+            Console.WriteLine(r.ToString());
+            Console.WriteLine(r4.ToString());
+
+            int i = r5;
+            Console.WriteLine(i.ToString());
+            i = r1;
+            Console.WriteLine(i.ToString());
+
+            float f = r1;
+            Console.WriteLine(f.ToString());
+
+
         }
     }
 }
---
+++ Lesson5/Lesson5/Rational.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    public sealed class Rational
+    {
+        private readonly int _numerator;
+        private readonly int _denomenator;
+
+        public Rational(int numerator, int denomenator)
+        {
+            _numerator = numerator;
+            _denomenator = denomenator;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            Rational m = obj as Rational;
+            if (m as Rational == null)
+                return false;
+
+            if (m._denomenator != this._denomenator)
+            {
+                return (this._numerator * m._denomenator == m._numerator * this._denomenator);
+            }
+            return (this._numerator == m._numerator);
+
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_numerator, _denomenator);
+
+        }
+
+        public static Rational operator +(Rational rational1, Rational rational2)
+        {
+            if (rational1._denomenator != rational2._denomenator)
+            {
+                return new Rational(rational1._numerator * rational2._denomenator + rational2._numerator * rational1._denomenator, rational1._denomenator * rational2._denomenator);
+            }
+            return new Rational(rational1._numerator + rational2._numerator, rational1._denomenator);
+        }
+
+        public static Rational operator -(Rational rational1, Rational rational2)
+        {
+            if (rational1._denomenator != rational2._denomenator)
+            {
+                return new Rational(rational1._numerator * rational2._denomenator - rational2._numerator * rational1._denomenator, rational1._denomenator * rational2._denomenator);
+            }
+            return new Rational(rational1._numerator - rational2._numerator, rational1._denomenator);
+        }
+
+        public static Rational operator *(Rational rational1, Rational rational2)
+        { 
+            return new Rational(rational1._numerator * rational2._numerator, rational1._denomenator * rational2._denomenator);
+        }
+
+        public static Rational operator /(Rational rational1, Rational rational2)
+        {
+           
+            return new Rational(rational1._numerator * rational2._denomenator, rational1._denomenator * rational2._numerator);
+        }
+
+        public static bool operator >(Rational rational1, Rational rational2)
+        {
+            if (rational1._denomenator != rational2._denomenator)
+            {
+                return rational1._numerator * rational2._denomenator > rational2._numerator * rational1._denomenator;
+            }
+            return rational1._numerator > rational2._numerator;
+        }
+
+        public static bool operator >=(Rational rational1, Rational rational2)
+        {
+            if (rational1._denomenator != rational2._denomenator)
+            {
+                return rational1._numerator * rational2._denomenator >= rational2._numerator * rational1._denomenator;
+            }
+            return rational1._numerator >= rational2._numerator;
+        }
+
+        public static bool operator <(Rational rational1, Rational rational2)
+        {
+            if (rational1._denomenator != rational2._denomenator)
+            {
+                return rational1._numerator * rational2._denomenator < rational2._numerator * rational1._denomenator;
+            }
+            return rational1._numerator < rational2._numerator;
+        }
+
+        public static bool operator <=(Rational rational1, Rational rational2)
+        {
+
+            if (rational1._denomenator != rational2._denomenator)
+            {
+                return rational1._numerator * rational2._denomenator <= rational2._numerator * rational1._denomenator;
+            }
+            return rational1._numerator < rational2._numerator;
+        }
+
+        public static Rational operator ++(Rational rational1)
+        {
+            return new Rational(rational1._numerator + rational1._denomenator, rational1._denomenator);
+        }
+
+
+        public  static Rational operator --(Rational rational1)
+        {
+            return new Rational(rational1._numerator - rational1._denomenator, rational1._denomenator);
+        }
+
+        public override string? ToString()
+        {
+            return string.Concat(_numerator, '/', _denomenator);
+        }
+
+        public static implicit operator int(Rational rational1)
+        {
+            return Convert.ToInt32(rational1._numerator/ rational1._denomenator);
+        }
+
+        public static implicit operator float(Rational rational1)
+        {
+            return (float)rational1._numerator / rational1._denomenator;
+        }
+
+
+
+    }
+}
