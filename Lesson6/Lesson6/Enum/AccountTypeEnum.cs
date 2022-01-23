ac67f73ac77927c8878754edbc07239c9ef462ee
Artur Stepanyants <alfacentaur@yandex.ru>
2022-01-23 15:33:41 +0300

    lesson6 mod 2

---
+++ Lesson6/Lesson6/Enum/AccountTypeEnum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6
+{
+    public enum AccountTypeEnum
+    {
+        Checking,
+        Payment,
+        Brokerage,
+        Investing,
+        Buget,
+        Personal,
+        Deposit,
+        Correspondent
+    }
+}
--- Lesson6/Lesson6/Program.cs
+++ Lesson6/Lesson6/Program.cs
@@ -6,7 +6,23 @@ namespace Lesson6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BankAccount bankAccountChecking = new BankAccount(AccountTypeEnum.Checking, 0);
+            BankAccount bankAccountDeposit = new BankAccount(AccountTypeEnum.Deposit, 1000);
+            BankAccount bankAccountPayment = new BankAccount(AccountTypeEnum.Payment, 5000);
+
+            Console.WriteLine(bankAccountChecking.ToString());
+            Console.WriteLine(bankAccountDeposit.ToString());
+            Console.WriteLine(bankAccountPayment.ToString());
+
+            Console.WriteLine(bankAccountChecking.Equals(bankAccountChecking));
+            Console.WriteLine(bankAccountChecking.Equals(bankAccountDeposit));
+
+            Console.WriteLine(bankAccountChecking == bankAccountChecking);
+            Console.WriteLine(bankAccountChecking == bankAccountDeposit);
+
+            Console.WriteLine(bankAccountChecking != bankAccountChecking);
+            Console.WriteLine(bankAccountChecking !=bankAccountDeposit);
+
         }
     }
 }
