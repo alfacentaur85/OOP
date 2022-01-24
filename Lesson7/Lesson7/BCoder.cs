6065275a43b8c86c5090973abec09ac0cde21185
Artur Stepanyants <alfacentaur@yandex.ru>
2022-01-24 11:48:24 +0300

    Lesson7

---
+++ Lesson7/Lesson7/BCoder.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lesson7.Interfaces;
+using Lesson7.Core;
+
+namespace Lesson7 
+{
+    public sealed class BCoder : ICoder
+    {
+        public string Decode(string textForEncoding)
+        {
+            textForEncoding = textForEncoding.ToUpper();
+
+            string result = "";
+
+            int oldOrder = 0;
+
+            int newOrder = 0;
+
+            int strLength = textForEncoding.Length;
+            for (int i = 0; i < strLength; i++)
+            {
+                oldOrder = Core.Core.Alphabet[textForEncoding[i]];
+
+                newOrder = Core.Core.Alphabet.Count - oldOrder + 1;
+
+                foreach (KeyValuePair<char, int> kvp in Core.Core.Alphabet)
+                {
+                    if (kvp.Value == newOrder)
+                    {
+                        result += kvp.Key;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+       public string Encode(string textForEncoding)
+        {
+            textForEncoding = textForEncoding.ToUpper();
+
+            string result = "";
+
+            int oldOrder = 0;
+
+            int newOrder = 0;
+
+            int strLength = textForEncoding.Length;
+            for (int i = 0; i < strLength; i++)
+            {
+                oldOrder = Core.Core.Alphabet[textForEncoding[i]];
+
+                newOrder = Core.Core.Alphabet.Count - oldOrder + 1;
+
+                foreach (KeyValuePair<char, int> kvp in Core.Core.Alphabet)
+                {
+                    if (kvp.Value == newOrder)
+                    {
+                        result += kvp.Key;
+                        break;
+                    }
+                }
+            }
+            return result;
+
+        }
+    }
+}
---
+++ Lesson7/Lesson7/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lesson7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*ACoder class test*/
+            Console.WriteLine("#--ACoder class test--#");
+
+            ACoder aCoder = new ACoder();
+
+            string sourceText = "Артур";
+            Console.WriteLine("Source text: {0}", sourceText);
+
+            string сoderEncodedText = aCoder.Encode(sourceText);
+            Console.WriteLine("Encoded text: {0}", сoderEncodedText);
+
+            string сoderDecodedText = aCoder.Decode(сoderEncodedText);
+            Console.WriteLine("Decoded text: {0}", сoderDecodedText);
+
+            /*BCoder class test*/
+            Console.WriteLine("\n\n#--BCoder class test--#");
+
+            BCoder bCoder = new BCoder();
+
+            sourceText = "АРТУР";
+            Console.WriteLine("Source text: {0}", sourceText);
+
+            сoderEncodedText = bCoder.Encode(sourceText);
+            Console.WriteLine("Encoded text: {0}", сoderEncodedText);
+
+            сoderDecodedText = bCoder.Decode(сoderEncodedText);
+            Console.WriteLine("Decoded text: {0}", сoderDecodedText);
+
+            Console.Read();
+
+
+        }
+    }
+}
